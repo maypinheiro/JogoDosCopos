5f53d97a2e6edb21dd0c35bca9a86d412eb95601
Mayara Pinheiro <mayarapinheirodesousa@hotmail.com>
2022-09-01 17:19:19 -0300

    Jogo dos Copos

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JogoDosCopos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            char posicao = char.Parse(Console.ReadLine().ToUpper());
+
+            for (int i = 0; i < numero; i++)
+            {
+                int troca = int.Parse(Console.ReadLine());
+
+                if (troca == 1 && (posicao == 'A' || posicao == 'B'))
+                    posicao = (posicao == 'A' ? 'B' : 'A');
+                else if (troca == 2 && (posicao == 'B' || posicao == 'C'))
+                    posicao = (posicao == 'B' ? 'C' : 'B');
+                else if (troca == 3 && (posicao == 'A' || posicao == 'C'))
+                    posicao = (posicao == 'C' ? 'A' : 'C');
+            }
+
+            Console.WriteLine(posicao); 
+        }
+    }
+}
